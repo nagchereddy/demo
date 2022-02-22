b107392a2840976d768d01c8b3fd425d10627143
U-JCPLC\jcheren <jcheren@M5277975.Go.JohnsonControls.com>
2022-02-22 14:17:50 +0530

    updated to demo

---
+++ src/main/java/Hello.java
@@ -0,0 +1,16 @@
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class Hello {
+	
+	@GET  
+	  @Produces(MediaType.TEXT_HTML)  
+	  public String sayPlainTextHello() {  
+	    return "Hello Jersey Plain";  
+	  }  
+
+}
